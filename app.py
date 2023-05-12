c2fa4acf43772d3f3cacae764c102c07664cc44b
Matthew Heeter <matthewheeter1611@gmail.com>
2023-05-12 11:46:59 -0400

    Added ability to run script directly and an about page

--- app.py
+++ app.py
@@ -12,7 +12,19 @@ app = Flask(__name__)
 # Routes are what are typed into browser to go to different pages (home, about, etc.)
 # / says this is the root or home page of the website
 @app.route("/") # Called a decorator, specifically the route decorator
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def home():
+    return "<h1>Home Page</h1>"
 # Need to run "set FLASK_APP=flaskblog.py" before running - Nevermind, just set the file name to app.py
-# To run use: "flask run" - still need to do this
+# To run use: "flask run" - still need to do this - After adding below conditional, just run using typical run button
+
+# Adding an about route
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+# Only true when this script is run directly, as stated above
+if __name__ == "__main__":
+    # Running in debug mode so that changes take effect without restarting web server
+    app.run(debug=True)
+    
+
